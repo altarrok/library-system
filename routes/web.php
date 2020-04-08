1ad1f341be763fe6080b56aecb5207315023d468
altay batuhan <altaybatuhanmail@gmail.com>
2020-04-07 21:05:12 -0700

    Landing page renewed Login redirects added

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+    if (auth()->user()) {
+        return redirect(route('home'));
+    }
     return view('welcome');
 });
 
